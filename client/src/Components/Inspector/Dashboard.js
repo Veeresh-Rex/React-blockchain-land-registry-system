9e80482d69432d313ee96179829e9c67eb58682c
veeresh.maurya <veeresh.maurya@impledge.com>
2022-04-25 23:07:58 +0530

    inspector dashboard setup

--- client/src/Components/Inspector/Dashboard.js
+++ client/src/Components/Inspector/Dashboard.js
@@ -1,7 +1,67 @@
-export function Dashboard() {
+import { useEffect, useState } from 'react';
+
+export function Dashboard(props) {
+     const { account, contract } = props;
+     const [inspectorDeatils, setInspectorDetails] = useState(null);
+     const [allUser, setAllUser] = useState(0);
+     const [allRegisteredUser, setAllRegisteredUser] = useState(0);
+     const [allLands, setAllLands] = useState(0);
+
+     useEffect(() => {
+          contract.methods
+               .InspectorMapping(account)
+               .call()
+               .then((result) => {
+                    setInspectorDetails(result);
+               });
+          contract.methods
+               .ReturnAllUserList()
+               .call()
+               .then((res) => {
+                    res.map((user) => {
+                         return contract.methods
+                              .UserMapping(user)
+                              .call()
+                              .then((result) => {
+                                   if (result.isUserVerified) {
+                                        setAllRegisteredUser(
+                                             allRegisteredUser + 1
+                                        );
+                                   }
+                              });
+                    });
+                    setAllUser(res.length);
+               });
+          contract.methods
+               .landsCount()
+               .call()
+               .then((res) => {
+                    setAllLands(res);
+               });
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+     }, [account, contract]);
+
      return (
           <>
-               <div>Dashboard</div>
+               <div className="title">
+                    Dashboard: {inspectorDeatils && inspectorDeatils.name}
+               </div>
+               <div className="section">
+                    <div className="columns is-variable is-4">
+                         <div class="column is-3 notification is-info mx-auto notification-height">
+                              <strong>Total user Registered</strong>
+                              <p class="subtitle">{allUser}</p>
+                         </div>
+                         <div class="column is-3 notification is-success mx-auto notification-height">
+                              <strong>Total user Verified</strong>
+                              <p class="subtitle">{allRegisteredUser}</p>
+                         </div>
+                         <div class="column is-3 notification is-warning mx-auto notification-height">
+                              <strong>Total lands</strong>
+                              <p class="subtitle">{allLands}</p>
+                         </div>
+                    </div>
+               </div>
           </>
      );
 }
